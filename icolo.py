4cb218a60f78efc3ee01b69fa6b141c5111e48fd
maieul <maieul@d2f3f259-0d32-4a7e-a242-bdbd99eb2334>
2010-06-18 20:55:05 +0000

    5 première regles

--- icolo.py
+++ icolo.py
@@ -15,10 +15,28 @@ equipe = {
 #fonction de calcul des points
 def calcul_point(equipe) :
 	points = 0
-	#etape 1 : Chaque personne canadienne a une valeur de base de 4 points
-	points = points + 4 * equipe['canadiens']
 	
+	#1 Chaque personne canadienne a une valeur de base de 4 points
+	points = points + 4 * equipe['canadiens']
 	affiche_points(1,points)
+	
+	#2 : Chaque personne suisse a une valeur de base de 6 points
+	points = points + 6 * equipe['suisses']
+	affiche_points(2,points)
+	
+	#3 : Chaque personne belge a la même valeur qu'une personne canadienne plus 1 point (= 5)
+	points = points + 5 * equipe['belges']
+	affiche_points(3,points)
+	
+	#4 : Chaque personne française a la même valeur qu'une personne canadienne plus 1 point (=5)
+	points = points + 5 * equipe['points']
+	affiche_points(4,equipe)
+	
+	#5 Si votre équipe comporte des personnes de chaque nationalité, votre nombre de points est doublé
+	
+	
+	
+	affiche_points(4,points)
 	
 def affiche_points(etape,points):
 	print ("etape "+str(etape)+" : "+str(points)+" points")
